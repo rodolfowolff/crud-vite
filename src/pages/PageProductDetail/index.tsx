4b9bb6e66b93e2d5ff25e9b6dd6c168b204cb4b5
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-09-21 15:15:40 -0300

    feat: initial page product detail get api infos

--- src/pages/PageProductDetail/index.tsx
+++ src/pages/PageProductDetail/index.tsx
@@ -1,11 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { getProductByID } from "../../services/products/getProductByID";
 
 export default function PageProductDetail() {
+  let { id } = useParams<"id">();
+  const { data: dataProduct, isLoading, isError } = getProductByID({ id });
+
+  if (isLoading) return <>Loading...</>;
+  if (isError) return <>Error(:</>;
+
   return (
     <div style={{ height: 400, width: "100%" }}>
-      <h2>Detalhes do produto</h2>
+      <h2>Detalhes do produto {id}</h2>
+
+      <p>{dataProduct.data.getProductByID.name}</p>
+      <p>{dataProduct.data.getProductByID.code}</p>
+      <p>{dataProduct.data.getProductByID.createdAt}</p>
+      <p>{dataProduct.data.getProductByID.category.name}</p>
       <p>
-        <Link to="/">Go to the home page</Link>
+        <Link to="/products">Retornar a lista de produtos</Link>
       </p>
     </div>
   );
