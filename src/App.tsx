2dc420bde3b93ca02d74cb6c44e8be9295ec99e5
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-09-21 11:22:12 -0300

    feat: routes home, 404 and product detail

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 
 import CustomMenu from "./components/Menu";
+import Home from "./pages/Home";
 import PageProducts from "./pages/Products";
+import PageProductDetail from "./pages/PageProductDetail";
 import PageCategories from "./pages/Categories";
+import NoMatch from "./pages/404";
 
 export default function App() {
   return (
     <SnackbarProvider maxSnack={3}>
       <CustomMenu>
         <Routes>
-          <Route path="/products" element={<PageProducts />} />
-          <Route path="/categories" element={<PageCategories />} />
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<PageProducts />} />
+          <Route path="product/:id" element={<PageProductDetail />} />
+          <Route path="categories" element={<PageCategories />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </CustomMenu>
     </SnackbarProvider>
