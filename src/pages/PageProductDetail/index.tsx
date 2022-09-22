22cc6829be7a77727f5591a799b13bb226220508
rodolfowolff <rodolfoluizwolff@gmail.com>
2022-09-22 04:06:23 -0300

    feat: improved the page details product

--- src/pages/PageProductDetail/index.tsx
+++ src/pages/PageProductDetail/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { format } from "date-fns";
 import { Link, useParams } from "react-router-dom";
 import FallbackSpinner from "../../components/Spinner";
 import { getProductByID } from "../../services/products/getProductByID";
@@ -10,16 +19,133 @@ export default function PageProductDetail() {
   if (isError) return <>Error(:</>;
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
-      <h2>Detalhes do produto {id}</h2>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingX: 10,
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{ mr: 2, fontWeight: 600, color: "text.primary" }}
+        >
+          {`Detalhes do produto`}
+        </Typography>
+      </Box>
 
-      <p>{dataProduct.data.getProductByID.name}</p>
-      <p>{dataProduct.data.getProductByID.code}</p>
-      <p>{dataProduct.data.getProductByID.createdAt}</p>
-      <p>{dataProduct.data.getProductByID.category.name}</p>
-      <p>
-        <Link to="/products">Retornar a lista de produtos</Link>
-      </p>
-    </div>
+      <Box sx={{ mt: 1, display: "flex", alignItems: "center", paddingX: 10 }}>
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 660,
+            position: "relative",
+            overflow: "auto",
+          }}
+        >
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  <Typography variant="body2">Nome</Typography>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="h6"
+                  >
+                    {dataProduct.data.getProductByID.name}
+                  </Typography>
+                </>
+              }
+              primaryTypographyProps={{
+                fontSize: 15,
+                fontWeight: "medium",
+                lineHeight: "20px",
+                mb: "2px",
+              }}
+            />
+          </ListItem>
+          <Divider />
+
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  <Typography variant="body2">Código</Typography>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="h6"
+                  >
+                    {dataProduct.data.getProductByID.code}
+                  </Typography>
+                </>
+              }
+              primaryTypographyProps={{
+                fontSize: 15,
+                fontWeight: "medium",
+                lineHeight: "20px",
+                mb: "2px",
+              }}
+            />
+          </ListItem>
+          <Divider />
+
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  <Typography variant="body2">Categoria</Typography>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="h6"
+                  >
+                    {dataProduct.data.getProductByID.category.name}
+                  </Typography>
+                </>
+              }
+              primaryTypographyProps={{
+                fontSize: 15,
+                fontWeight: "medium",
+                lineHeight: "20px",
+                mb: "2px",
+              }}
+            />
+          </ListItem>
+          <Divider />
+
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  <Typography variant="body2">Data de criação</Typography>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="h6"
+                  >
+                    {format(
+                      new Date(dataProduct.data.getProductByID.createdAt),
+                      "dd/MM/yyyy"
+                    )}
+                  </Typography>
+                </>
+              }
+              primaryTypographyProps={{
+                fontSize: 15,
+                fontWeight: "medium",
+                lineHeight: "20px",
+                mb: "2px",
+              }}
+            />
+          </ListItem>
+        </List>
+      </Box>
+      <Link to="/products">Retornar a lista de produtos</Link>
+    </>
   );
 }
